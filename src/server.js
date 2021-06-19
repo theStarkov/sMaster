07ef100a654abce48630364b42c1625513c39025
unknown <e.thompson@APPSNMOB-TEC-09.appsNmobile.local>
2021-06-19 12:09:43 +0000

    sockets

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ import express from 'express'
 import cookieParser from "cookie-parser";
 import cookieSession from "cookie-session";
 import cors from "cors";
+import { createServer } from 'http';
 import session from 'express-session'
+import socketio from 'socket.io'
 
 import 'dotenv/config'
 import db from './utils/db';
@@ -11,6 +13,7 @@ import db from './utils/db';
 import authRouter from './routes/auth'
 
 const app = express()
+const server = createServer(app)
 const port = process.env.PORT
 
 // test db connection
@@ -21,7 +24,7 @@ db.authenticate()
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-app.use(cors());
+// app.use(cors());
 app.use(session({
     secret: process.env.SECRET_KEY,
     resave: true,
@@ -32,4 +35,22 @@ app.use(cookieParser(process.env.SECRET_KEY))
 // routes
 app.use('/', authRouter)
 
-app.listen(port, () => console.log(`App running on ${port}`))
+// socket
+
+const io = socketio(server)
+var n = 0;
+
+setInterval(() => {
+    n = n + 1
+    console.log(n)
+}, 2000)
+
+io.on('connection', (socket) => {
+    console.log('Socket connected')
+
+    socket.on('send_data', () => {
+        socket.emit('receive_data', n)
+    })
+})
+
+server.listen(port, () => console.log('server on ' + port))
