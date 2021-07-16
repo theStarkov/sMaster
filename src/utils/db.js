05dec37d29a28984e2472397789e08d731662f37
thestarkov <eothompson@st.ug.edu.gh>
2021-07-16 10:50:46 +0000

    changed db

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize'
 
-const db = new Sequelize('postgres://postgres:root@localhost:5432/hdDB')
+const db = new Sequelize('postgres://yliqdzke:os2gIxi5KxqxslW9ejG9klOOA2wL857q@batyr.db.elephantsql.com/yliqdzke')
 
 export default db
