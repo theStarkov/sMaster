04b411f161f8986cadd20a6053716a6cec8c6304
thestarkov <eothompson@st.ug.edu.gh>
2021-09-17 03:31:31 +0000

    added device db

---
+++ src/models/Device.js
@@ -0,0 +1,18 @@
+import Sequelize from 'sequelize'
+import db from '../utils/db'
+
+const Device = db.define('device', {
+    uuid: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
+      },
+    deviceId: Sequelize.STRING,
+    activated: Sequelize.BOOLEAN,
+   
+})
+
+Device.sync({alter: true})
+    .then(() => console.log('Device table created....'))
+
+export default Device 
